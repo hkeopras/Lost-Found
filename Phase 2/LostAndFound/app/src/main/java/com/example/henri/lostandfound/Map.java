a26eaecf9a5bcee6caf82a17dae26cd8ea9c1f6c
Henri Keopraseuth <henri.keopraseuth@unil.ch>
2018-03-22 20:51:50 +0100

    First front-end implementation (not finished)

---
+++ Phase 2/LostAndFound/app/src/main/java/com/example/henri/lostandfound/Map.java
@@ -0,0 +1,25 @@
+package com.example.henri.lostandfound;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Henri on 22.03.2018.
+ */
+
+public class Map extends Fragment {
+
+    View myView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.activity_map, container, false);
+        return myView;
+    }
+
+}
---
+++ Phase 2/LostAndFound/app/src/main/java/com/example/henri/lostandfound/Settings.java
@@ -0,0 +1,25 @@
+package com.example.henri.lostandfound;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Henri on 22.03.2018.
+ */
+
+public class Settings extends Fragment {
+
+    View myView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.activity_settings, container, false);
+        return myView;
+    }
+
+}
